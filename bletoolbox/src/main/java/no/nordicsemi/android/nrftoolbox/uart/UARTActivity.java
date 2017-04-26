eaecb2283c5da76a87651253445da4a85c0d7260
Jungchae Kim <jungchae.kim@lge.com>
2017-04-26 19:58:26 +0900

    Support for session control requirements
    :Detailed Notes:
    - Major issue is session control requirement
    - Minor issues are reducing bp input range and correcting DB export path
    
    :Issues Addressed:
    [BM-740] BP data acqusition app - Release 1.1
    
    Signed-off-by: Jungchae Kim <jungchae.kim@lge.com>

--- bletoolbox/src/main/java/no/nordicsemi/android/nrftoolbox/uart/UARTActivity.java
+++ bletoolbox/src/main/java/no/nordicsemi/android/nrftoolbox/uart/UARTActivity.java
@@ -810,6 +810,7 @@ public class UARTActivity extends BleProfileServiceReadyActivity<UARTService.UAR
 			writer.close();
 
 			// Notify user about the file
+			Toast.makeText(this, file.getName() + " is created.", Toast.LENGTH_SHORT).show();
 			/*
 			final Intent intent = new Intent(Intent.ACTION_VIEW);
 			intent.setDataAndType(FileHelper.getContentUri(this, file), "text/xml");
